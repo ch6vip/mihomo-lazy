6c43b2b89194899568078491f775a4d1a9da9c6f
美男子 <hubentuan@gmail.com>
2025-10-29 22:27:32 +0800

    Update 关闭开机启动.bat

--- 关闭开机启动.bat
+++ 关闭开机启动.bat
@@ -1,16 +1,19 @@
 @echo off
 chcp 65001 >nul
-echo 正在关闭 Clash 开机自启动...
+echo 正在关闭 Mihomo 开机自启动...
+echo.
 
-:: 删除开机启动快捷方式
+:: 开机启动文件夹
 set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "shortcut=%startup%\Clash.lnk"
+set "shortcut=%startup%\Mihomo.lnk"
 
+:: 删除快捷方式
 if exist "%shortcut%" (
     del "%shortcut%"
-    echo Clash 开机自启动已关闭！
+    echo ✓ Mihomo 开机自启动已关闭！
 ) else (
-    echo Clash 未设置开机自启动！
+    echo × Mihomo 未设置开机自启动
 )
 
-timeout /t 3 /nobreak >nul
+echo.
+pause
